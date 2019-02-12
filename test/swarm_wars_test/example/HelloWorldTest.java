18b4a2f9c2eed2ddafff7ea1e58d0f15d684999f
lao8n <hellolao8n@gmail.com>
2019-02-12 20:42:50 +0000

    ant_build commit

---
+++ test/swarm_wars_test/example/HelloWorldTest.java
@@ -0,0 +1,17 @@
+package swarm_wars_test.example;
+
+import org.junit.Test;
+
+import static org.junit.Assert.fail;
+
+public class HelloWorldTest {
+
+    @Test
+    public void testNothing() {
+    }
+
+    @Test
+    public void testWillAlwaysFail() {
+        fail("An error message");
+    }
+}
